a34f34823d86890df5592be02458f11b8a5751da
Kody Buss <kody.buss@zelis.com>
2024-12-27 23:33:02 -0700

    added make file, github ci workflows

--- new-ml-inference-service/new-ml-inference-service/new_ml_inference_service/api/prediction.py
+++ new-ml-inference-service/new-ml-inference-service/new_ml_inference_service/api/prediction.py
@@ -1,19 +1,35 @@
-from flask import Blueprint, request
+from flask import abort, Blueprint, request
+from pydantic import ValidationError
 
 from schemas.apartment import Apartment
-from services.model_inference_service import ModelInferenceService
+from services import model_inference_service
 
 bp = Blueprint('prediction', __name__, url_prefix='/pred')
 
 
 @bp.get('/')
 def get_prediction() -> dict:
-    # get and check input parameters (feature values)
-    apartment_features = Apartment(**request.args)
 
-    # load an existing ML model
-    model_inference_service = ModelInferenceService()
-    model_inference_service.load_model()
+    try:  # get and check input parameters (feature values)
+        apartment_features = Apartment(**request.args)
+    except ValidationError:
+        abort(code=400, description='Bad input error')
+
+    # feed input parameters to the loaded ML model to get a prediction
+    prediction = model_inference_service.predict(
+        list(apartment_features.model_dump().values()),
+    )
+
+    # return a prediction value
+    return {'prediction': prediction}
+
+
+@bp.post('/')
+def get_prediction_post() -> dict:
+    try:  # get and check input parameters (feature values)
+        apartment_features = Apartment(**request.json)
+    except ValidationError:
+        abort(code=400, description='Bad input error')
 
     # feed input parameters to the loaded ML model to get a prediction
     prediction = model_inference_service.predict(
---
+++ new-ml-inference-service/new-ml-inference-service/new_ml_inference_service/services/__init__.py
@@ -0,0 +1,4 @@
+from services.model_inference_service import ModelInferenceService
+
+model_inference_service = ModelInferenceService()
+model_inference_service.load_model()
